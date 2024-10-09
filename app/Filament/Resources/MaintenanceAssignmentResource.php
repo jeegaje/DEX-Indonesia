ca25938b61e6fb0b856992c949c7189223de01e9
anggajiyan.imanuddin <anggajiyanfajar@gmail.com>
2024-10-10 03:38:23 +0700

    [ADD] Create Maintenance Assignment Module

---
+++ app/Filament/Resources/MaintenanceAssignmentResource.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MaintenanceAssignmentResource\Pages;
+use App\Filament\Resources\MaintenanceAssignmentResource\RelationManagers;
+use App\Models\MaintenanceAssignment;
+use App\Models\Pump;
+use App\Models\Role;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MaintenanceAssignmentResource extends Resource
+{
+    protected static ?string $model = MaintenanceAssignment::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationLabel = 'Penugasan  Maintenance';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->label('nama')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->label('Email')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('password')
+                    ->label('Password')
+                    ->numeric()
+                    ->required(),
+                Select::make('role_id')
+                    ->label('Role')
+                    ->options(Role::where('id', '!=', 1)->pluck('name', 'id'))
+                    ->searchable(),
+                Repeater::make('pumps')
+                    ->label('Data Pompa')
+                    ->schema([
+                        Select::make('pump_id')
+                            ->label('No Seri Pompa')
+                            ->searchable()
+                            ->getSearchResultsUsing(function (string $search): array {
+                                return Pump::where('serial_number', 'like', "%{$search}%")
+                                    ->orWhere('location', 'like', "%{$search}%")
+                                    ->limit(10)
+                                    ->get()
+                                    ->mapWithKeys(function ($pump) {
+                                        return [$pump->id => "{$pump->serial_number} ({$pump->location})"];
+                                    })
+                                    ->toArray();
+                            }),
+                        Select::make('maintenance_status')
+                            ->label('Status Maintenance')
+                            ->options([
+                                'monthly' => 'Maintenance Bulanan',
+                                'full' => 'Full Maintenance',
+                            ])
+                    ])
+                    ->columns(2)
+                    ->columnSpan([
+                        'sm' => 2,
+                    ])
+
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user.name')
+                    ->label('Name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('user.email')
+                    ->label('Email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('pump.serial_number')
+                    ->label('Pump Serial Number')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('user.role.name')
+                    ->badge()
+                    ->color(fn (string $state): string => match ($state) {
+                        'Manajemen' => 'warning',
+                        'Internal' => 'success',
+                        'Teknisi' => 'danger',
+                    })
+                    ->searchable(),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMaintenanceAssignments::route('/'),
+            'create' => Pages\CreateMaintenanceAssignment::route('/create'),
+            'edit' => Pages\EditMaintenanceAssignment::route('/{record}/edit'),
+        ];
+    }
+}
