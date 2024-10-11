71119e87db7aab74e0e2fcd31702347c99aa2a2a
anggajiyan.imanuddin <anggajiyanfajar@gmail.com>
2024-10-11 07:28:03 +0700

    [ADD] Add Maintenance model

---
+++ database/migrations/2024_10_09_205857_create_maintenances_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('maintenances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('maintenance_assignment_id')->constrained('maintenance_assignments')->onDelete('cascade');
+            $table->string('maintenance_type');
+            $table->string('maintenance_status');
+            $table->date('inspection_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('maintenances');
+    }
+};
