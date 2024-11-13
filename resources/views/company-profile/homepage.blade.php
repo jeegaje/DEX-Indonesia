1c89a15279c4c7f9cfea731b08d20b7c6e7f5cc8
anggajiyan.imanuddin <anggajiyanfajar@gmail.com>
2024-11-13 12:03:39 +0700

    [UPDATE] Create Template for page company profile

--- resources/views/company-profile/homepage.blade.php
+++ resources/views/company-profile/homepage.blade.php
@@ -1,40 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@extends('company-profile.template')
 
-        <title>Dex Pump - Bersama Membangun Indonesia</title>
-
-        @vite(['resources/css/app.css','resources/js/app.js'])
-        @livewireStyles
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-
-        <style>
-        .swiper {
-            width: 100%;
-            height: 300px;
-          }
-      
-          .swiper-slide {
-            text-align: center;
-            font-size: 18px;
-            background: #fff;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-          }
-      
-          .swiper-slide img {
-            display: block;
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-          }
-        </style>
-    </head>
-    <body>
-        <livewire:company-profile.navbar />
+@section('content')
         <section class="bg-white dark:bg-gray-900">
             <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                 <div class="mr-auto place-self-center lg:col-span-7">
@@ -380,21 +346,4 @@
                 </div>
             </div>
         </section>
-  
-  <!-- Main modal -->
-  
-        <livewire:company-profile.footer :company_profile="$company_profile" />
-    </body>
-    @livewireScripts
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script>
-        var swiper = new Swiper(".mySwiper", {
-            direction: "vertical",
-            navigation: {
-                nextEl: ".swiper-button-next-ya",
-                prevEl: ".swiper-button-prev-ya",
-            },
-            loop: true
-        });
-    </script>
-</html>
+@endsection
--- resources/views/company-profile/project.blade.php
+++ resources/views/company-profile/project.blade.php
@@ -1,41 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@extends('company-profile.template')
 
-        <title>Laravel</title>
-
-        @vite(['resources/css/app.css','resources/js/app.js'])
-        @livewireStyles
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-
-        <style>
-        .swiper {
-            width: 100%;
-            height: 300px;
-          }
-      
-          .swiper-slide {
-            text-align: center;
-            font-size: 18px;
-            background: #fff;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-          }
-      
-          .swiper-slide img {
-            display: block;
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-          }
-        </style>
-
-    </head>
-    <body>
-        <livewire:company-profile.navbar />
+@section('content')
         <section>
             <div class="max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-8 lg:grid-cols-12">
                 <div class="text-center">
@@ -131,19 +96,4 @@
                 </div>
             </div>
         </section>        
-        <livewire:company-profile.footer :company_profile="$company_profile" />
-    </body>
-    @livewireScripts
-
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script>
-        var swiper = new Swiper(".mySwiper", {
-            direction: "vertical",
-            navigation: {
-                nextEl: ".swiper-button-next-ya",
-                prevEl: ".swiper-button-prev-ya",
-            },
-            loop: true
-        });
-    </script>
-</html>
+@endsection
